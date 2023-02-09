85a2869ae80e95c742d3e0ae6a6ac1aa972eb67a
kyubhinhan <kyubhinhan@gmail.com>
2023-02-09 17:40:30 +0900

    Hot Fix : 로그아웃하고 다시 로그인 시 todolist 목록이 업데이트 되지 않는 문제 해결
    로그아웃하고 다시 로그인 시에 업데이트 안되는 것을 파악하여,
    로그아웃 때, 페이지가 새로고침되면서 이동하도록 변경해줌

--- src/components/LogoutIcon.tsx
+++ src/components/LogoutIcon.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { FiLogOut } from 'react-icons/fi';
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
 
 const LogoutIcon = () => {
-  const navigate = useNavigate();
-
   const handleLogoutButtonClick = () => {
     // access token 초기화
     localStorage.setItem('access_token', '');
-    navigate('/signin');
+    // signin page로 새로고침되면서 넘어가도록함
+    window.location.replace('/signin');
   };
 
   // access_token이 없는 경우 로그아웃 표시를 보여주지 않음
